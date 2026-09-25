168d822e98b7cdb9beede429cbe5dc6569cf25da
jorgen-es <jorgen.e.skalle@hotmail.no>
2026-09-25 14:48:32 +0200

    a)

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -5,9 +5,10 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
-
+		for(int i = 0; i < tabell.length; i++) {
+			System.out.print(tabell[i] + ", ");
+		}
+		System.out.println();
 	}
 
 	// b)
